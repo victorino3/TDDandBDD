9297e887dc62bc8dd7afa99607d606caa58c225e
victorino3 <victorinogomes1@gmail>
2023-02-25 23:39:46 +0000

    more case using TDD and BDD

--- services/service.js
+++ services/service.js
@@ -1,15 +1,42 @@
 const BaseRepository = require("../repository/base/baseRepository.js")
+const Tax = require("../src/entities/tax")
 
 class CarService{
     constructor({cars}){
         this.CarRepository = new BaseRepository({file:cars})
+        this.taxBaseOnCurrency = Tax.taxBaseOnAge
+        this.currentCurrency = new Intl.NumberFormat("de-DE",{
+            style:"currency",
+            currency:"EUR"
+        })
+
     }
     getRandomPositon(list){
         return Math.floor(Math.random() * list.length)
     }
+    chooseRandomVeycle(carCategory){
+        const randomIndexCar = this.getRandomPositon(carCategory.carId)
+        return carCategory.carId[randomIndexCar]
+
+    }
 
     test(id){
         return this.CarRepository.find(id)
+    }
+
+    async getAvailableCar(carCategory){
+        const randomVeycleId = this.chooseRandomVeycle(carCategory)
+        return await this.CarRepository.find(randomVeycleId);
+    }
+    calculateFinalPrice(costumer,carCategory,numberOfDay){
+        const {age} = costumer
+        const price = carCategory.price
+        const {then : tax } = this.taxBaseOnCurrency
+            .find(tax => age >= tax.from && age <= tax.to )
+
+        const finalPrice = ((tax * price ) * (numberOfDay))
+        const formattedPrice = this.currentCurrency.format(finalPrice)
+        return formattedPrice
     }
 }
 module.exports=CarService
--- test/uniTests/carServices.test.js
+++ test/uniTests/carServices.test.js
@@ -1,21 +1,84 @@
-const {describe, it} = require("mocha")
+const { describe, it, before, beforeEach, afterEach } = require("mocha")
 const service = require("../../services/service")
-const {join} = require("path")
-const {expect} = require("chai")
+const { join } = require("path")
+const { expect } = require("chai")
+const sinon = require("sinon")
 const databaseFiles = join(__dirname,"../../database/","cars.json")
+const mocks = {
+    valid_car: require("../mocks/valid-car.json"),
+    valid_carCategory: require("../mocks/valid-carCategory.json"),
+    valid_costumer: require("../mocks/valid-costumer.json")
+}
 describe("CarService Suite Test",()=>{
+    let sandbox = {}
     let carService = {}
+
     before(()=>{
          carService =  new service({cars:databaseFiles})
     })
+
+    beforeEach(()=>{
+        sandbox = sinon.createSandbox()
+    })
+
+    afterEach(()=>{
+        sandbox.restore()
+    })
+
     it("It should retrieve a random position form an array",()=>{
         const data = [0,1,2,3,4]
         const result = carService.getRandomPositon(data)
-        console.log(result)
         expect(result).to.be.lte(data.length).and.gte(0)
     })
+    it("Should choose the first id from cardIds in carCategory",()=>{
+        const mokc_carCategory = mocks.valid_carCategory;
+        const carIndex = 0
+
+        sandbox.stub(
+            carService,
+            carService.getRandomPositon.name
+        ).returns(carIndex)
+
+        const result = carService.chooseRandomVeycle(mokc_carCategory)
+        const expected = mokc_carCategory.carId[carIndex]
+        expect(result).to.be.deep.equal(expected)
+    })
+
     it("Given a car category, it should return an avalaible car",async()=>{
-        const result = await carService.test('27c40b42-b552-42f7-955b-7372eeccb2ff')
-        //console.log("result ", result)
+        const car = mocks.valid_car;
+        const carCategory = Object.create(mocks.valid_carCategory);
+        carCategory.carId = [car._id]
+
+        sandbox.stub(
+            carService.CarRepository,
+            carService.CarRepository.find.name,
+        ).resolves(car)
+
+        sandbox.spy(
+            carService,
+            carService.chooseRandomVeycle.name
+        )
+        
+        const result = await carService.getAvailableCar(carCategory);
+        const expected = car
+        expect(result).to.be.deep.equal(expected)
+        expect(carService.chooseRandomVeycle.calledOnce).to.be.ok
+        expect(carService.CarRepository.find.calledWithExactly(car._id))
+
+    })
+
+    it("Given a carCategory, costumer and numberOfDay it should calculate final amout in space of 5 days", async()=>{
+        const costumer = Object.create(mocks.valid_costumer)
+        costumer.age = 50
+
+        const carCategory = Object.create(mocks.valid_carCategory)
+        carCategory.price = 37.6
+
+        const numberOfDay = 5
+
+        const expected = carService.currentCurrency.format(244.40)
+        const result = carService.calculateFinalPrice(costumer,carCategory,numberOfDay)
+
+        expect(result).to.be.deep.equal(expected)
     })
 })
